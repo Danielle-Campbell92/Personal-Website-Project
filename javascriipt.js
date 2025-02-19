2bfb64eafe684044ea30b5f5fb7d034bc4e79b5e
Danielle <dani.camp1125@gmail.com>
2025-02-18 20:47:25 -0500

    New htmls and javascript added. Changed site styling.

--- javascriipt.js
+++ javascriipt.js
@@ -1,7 +1,17 @@
 //1.Create console.log with the string "Hello World!"
 //2.Ensure it is added to all pages.
+//3. Add details from Canvas.
 
 //Step 1 & 2:
 console.log('Hello World!');
-alert("Hello World!");
+
+//Step 3:
+const form = document.querySelector('#form')
+const submitButton = document.querySelector('#submit')
+
+form.addEventListener('submit', (e) => {
+  submitButton.disabled = true
+  e.preventDefault()
+  window.location.href = window.location.origin + '/success.html'
+})
 
